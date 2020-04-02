ed648d5081b8a97b2ed49c76a8a973e66f84c31f
Alexa <alexakearns@outlook.com>
2020-04-02 15:58:06 +0100

    enabled session and created /play

--- app.rb
+++ app.rb
@@ -1,16 +1,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
     #'Testing infrastructure working!'
   end
 
 post '/names' do
-  @first_name =params[:first_name]
-  @second_name =params[:second_name]
-  erb :play
+  session[:first_name] = params[:first_name]
+  session[:second_name] = params[:second_name]
+  redirect '/play'
 end 
+
+get '/play' do
+  @first_name = session[:first_name]
+  @second_name = session[:second_name]
+  erb :play
+end
 
   run! if app_file == $0
 
