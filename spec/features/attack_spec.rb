df07a72b5f6c38df0ca230abcfeacebc358c5c60
Alex L <macbookpro@macbooks-mbp.home>
2020-04-03 15:05:05 +0100

    test - Alex has lost 10 HP - fail

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,15 @@
-feature 'attack player' do 
+feature 'attack player' do
   scenario 'player 1 attacks player 2' do
     sign_in_and_play
     click_button 'Attack'
     expect(page).to have_content('Gina attacked Alexa')
-  end 
-end 
+  end
+end
+
+feature 'player 1 can attack player 2 and reduce HP by 10' do
+  scenario 'player 2 looses 10 HP' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content "player 2 has lost 10HP"
+  end
+end
